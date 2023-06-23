84469f83cb2757ca18c5e235b45679791561d5f1
aminerve <122647915+aminerve@users.noreply.github.com>
2023-06-23 11:26:30 -0400

    search functionality complete

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,46 +1,65 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
 export default function Home() {
-    const [movies, setMovies] = useState([])
-    const [text, setText] = useState('')
+  const [movies, setMovies] = useState([]);
+  const [text, setText] = useState("");
 
-    useEffect(() => {
-        const fetchMovieData = async() => {
-            try {
-                const res = await fetch(`https://www.omdbapi.com/?t=${text}&apikey=a8a7d5a7`)
-                const data = await res.json()
-                setMovies(data)
-                console.log(data)
-            } catch (error) {
-                console.error(error)
-            }
-        } 
-        fetchMovieData()
-    }, [text])
+  useEffect(() => {
+    const fetchMovieData = async () => {
+      try {
+        const res = await fetch(
+          `https://www.omdbapi.com/?t=${text}&apikey=a8a7d5a7`
+        );
+        const data = await res.json();
+        setMovies(data);
+        console.log(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchMovieData();
+  }, [text]);
 
-    const searchByName = async() => {
-        try {
-            const res = await fetch(`https://www.omdbapi.com/?t=${text}&apikey=a8a7d5a7`)
-            
-            if (!res.ok) throw new Error("Could not find Movie")
+  const searchByName = async () => {
+    try {
+      const res = await fetch(
+        `https://www.omdbapi.com/?t=${text}&apikey=a8a7d5a7`
+      );
 
-            const data = await res.json()
-            setMovies(data)
-        } catch (error) {
-            console.error(error)
-        }
+      if (!res.ok) throw new Error("Could not find Movie");
+
+      const data = await res.json();
+
+      if (data.Search){
+             setMovies(data); 
+      }
+
+    } catch (error) {
+      console.error(error);
     }
-    const handleSubmit = (e) => {
-        e.preventDefault()
+  };
+  const handleSubmit = (e) => {
+    if (e.key === 'enter'){
+    e.preventDefault();
 
-        searchByName()
+    searchByName();     
     }
+
+  };
   return (
     <div>
-        <h1>AllAboutThatMovie.com</h1>
-        <form onSubmit={handleSubmit}>
-            <input type='text' placeholder='Search your favorite movies!' value={text} onChange={(e) => setText(e.target.value)}/>
-        </form>
+      <h1>AllAboutThatMovie.com</h1>
+      <form>
+        <input
+          type="text"
+          placeholder="Search your favorite movies!"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          onKeyDown={handleSubmit}
+        />
+      </form>
+
+      
     </div>
-  )
+  );
 }
