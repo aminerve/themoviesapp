e20696fab08927190aecec5378753a1a0ec8bb60
aminerve <122647915+aminerve@users.noreply.github.com>
2023-06-23 10:37:46 -0400

    connected to api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home';
+import SingleMovie from './pages/SingleMovie';
 
 function App() {
   return (
     <div className="App">
- 
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/:name' element={<SingleMovie/>}/>
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from 'react'
+
+export default function Home() {
+    const [movies, setMovies] = useState([])
+    const [text, setText] = useState('')
+
+    useEffect(() => {
+        const fetchMovieData = async() => {
+            try {
+                const res = await fetch(`https://www.omdbapi.com/?t=${text}&apikey=a8a7d5a7`)
+                const data = await res.json()
+                setMovies(data)
+                console.log(data)
+            } catch (error) {
+                console.error(error)
+            }
+        } 
+        fetchMovieData()
+    }, [text])
+
+    const searchByName = async() => {
+        try {
+            const res = await fetch(`https://www.omdbapi.com/?t=${text}&apikey=a8a7d5a7`)
+            
+            if (!res.ok) throw new Error("Could not find Movie")
+
+            const data = await res.json()
+            setMovies(data)
+        } catch (error) {
+            console.error(error)
+        }
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        searchByName()
+    }
+  return (
+    <div>
+        <h1>AllAboutThatMovie.com</h1>
+        <form onSubmit={handleSubmit}>
+            <input type='text' placeholder='Search your favorite movies!' value={text} onChange={(e) => setText(e.target.value)}/>
+        </form>
+    </div>
+  )
+}
